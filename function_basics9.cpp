e7fea5f20f49114ec8094da5689c7f18ccb442e8
algohercules <adarshrreso@gmail.com>
2024-12-31 20:16:37 +0530

    test

---
+++ function_basics9.cpp
@@ -0,0 +1,32 @@
+  //function is a peogram having well defined  task
+
+  #include <iostream>
+  using namespace std;
+
+  int power(int a, int b){
+  
+  int ans = 1;
+
+  for (int i = 1; i <= b, i++ ;){
+  ans = ans * a;
+
+  return ans;
+  }
+
+  }
+
+
+  int main () {
+
+   int a , b;
+   cin >> a >> b;
+
+   power(a,b);
+
+   return 0;
+
+
+
+
+
+  }
