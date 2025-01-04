d725f6eb36b23fa506dbee075bbc038125b2fabf
algohercules <adarshrreso@gmail.com>
2025-01-04 20:02:51 +0530

    arrays are op

--- arrays_op1.cpp
+++ arrays_op1.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+void printarray( ) {
 
-int number[15];
+}
 
-cout <<  "everything is fine " <<endl;
+int main() {
 
+    //declare
+    int number[15];
+    //accessing an arary
+    cout << "value of 14th index is" << number[14] << endl;
 
+     //initialising an array
+     int second[3] = {5 , 7 , 11};
 
+     //accessing
+    cout << "value of 2nd index is" << second[2] << endl;
 
+     int third[15] = {2,7};
 
+     int n = 15;
 
+     cout << "printing the array" <<endl;
 
-
-
-
+for(int i = 0; i <= n; i++){
+cout << third[i] <<" ";
+}
 
 }
--- functions_basics2.cpp
+++ functions_basics2.cpp
@@ -4,13 +4,24 @@
 using namespace std;
 
 int add( int num1 , int num2 ){
-
     int sum = num1 + num2;
-
     return sum;
 }
 
+ int add( int num1, int num2, int num3){
+    int sum = num1 + num2 + num3;
+    return sum;
+ }
+
+ int add(float num1, float num2, float num3){
+    float sum = num1 + num2 + num3;
+    return sum;
+ }
+
+
 int main(){
+
+// ab hum jaise jaise cout ki vlaues change usi hisab se functions call honge aur ouput aayega
 
 int a = 5;
 int b = 7;
