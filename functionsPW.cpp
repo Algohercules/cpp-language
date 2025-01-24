8207fefa79eda529dcac3edddb07e6ffb84227fe
algohercules <adarshrreso@gmail.com>
2025-01-24 18:45:59 +0530

    properties of functions

--- functionsPW.cpp
+++ functionsPW.cpp
@@ -18,9 +18,9 @@ int add(int num1 , int num2, int num3){
 
 int main(){
 
-// ab hum jaise jaise cout ki vlaues change usi hisab se functions call honge aur ouput aayega
+// ab hum jaise jaise cout ki vlaues change usi hisab se functions call honge aur output aayega
 
-int a = 5;
+int a = 5
 int b = 7;
 
  cout << add (a , b , 2) <<endl;
@@ -36,6 +36,7 @@ if we want to define a function after the function call, we need to use the func
 
 // pow function:--
 
+/*
 int main(){
 
    cout << pow(2,3) <<endl;
@@ -48,5 +49,50 @@ int main(){
    cout << doubleans <<endl;
 
    }
+   */
 
-   
+//scope of variables :--
+
+
+/*
+int main(){
+     int apples = 5;
+     cout << apples;
+}
+
+
+
+//the value of variable apple is only accessible in the curly brackets of int main function, not outside of it 
+*/
+
+/*
+
+
+
+for (int i=0; i<=n; i++){
+ cout << i; -->i will be printed
+}
+cout << i;  --> error
+  
+
+
+* two variables in same scope with same name is not allowed.
+
+*two variables with same name but in two different scopes are alowed
+
+
+
+int main(){
+
+int p = 5;
+while(){
+float p =5.7;
+cout << p;  -->5.7
+}
+
+cout <<p; -->5
+}
+
+
+
+*/
